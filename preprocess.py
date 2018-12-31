e00959eef3de740e634ef8b3aee52a2278bc1fcb
Jeonghwan <jeonghwankim123@gmail.com>
2019-01-01 02:11:55 +0900

    Update preprocess.py with sentence2tensor

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,61 @@
-import torch
-import torch.nn as nn
 import os.path
 import glob as g
 import unicodedata
+import torch
+import numpy as np
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 PAR_DIR='.\data'
+SOS_token = '<\s>'
+EOS_token = '<\e>'
+
+class Language():
+    def __init__(self, lang='UNK'):
+        self.lang = lang
+        self.sentences = []
+        self.word2cnt = {}
+        self.word2idx = {SOS_token: 0, EOS_token: 1}
+        self.idx2word = {0: SOS_token, 1: EOS_token}
+        self.num_words = 2
+    
+    def parseSentence(self, sentence):
+        sentence_indices = []
+        self.sentences = sentence.split('\n')
+        for s in self.sentences:
+            for word in s.split():
+                if word not in self.word2idx:
+                    self.word2idx[word] = self.num_words
+                    self.word2cnt[word] = 1
+                    self.idx2word[self.num_words] = word
+                    self.num_words += 1
+                else:
+                    self.word2cnt[word] += 1
+
+    def maxLen(self):
+        max_seq_len = 0
+        for s in self.sentences:
+            length = len(s.split())
+            max_seq_len = max(max_seq_len, length)
+        return max_seq_len
+
+    def sentence2tensor(self, max_seq_len):
+        sent_tensor = []
+        cnt = 0
+        for s in self.sentences:
+            indices = []
+            s += ' ' + EOS_token # Append EOS token at the end of each sentence
+            for word in s.split():
+                indices.append(self.word2idx[word])
+            sent_tensor.append(indices)
+        print(len(sent_tensor))
+        return torch.tensor(sent_tensor, dtype=torch.long, device=device).view(len(sent_tensor), max_seq_len)
+
+    def langName(self):
+        return self.lang
 
 class DataProcess():
     def __init__(self, path=PAR_DIR):
         self.path = path
-        self.eng_word2cnt = {}
-        self.fra_word2cnt = {}
-        self.idx2word = {0: 'SOS', 1: 'EOS'}
 
     def read_data(self):
         # Read data from the specified path
@@ -25,54 +69,25 @@ class DataProcess():
             print('File open failed: ', e.strerror)
         return data
 
-    def getCount(self, dict_data):
-        return dict_data[1]
-
-    def build_dict(self, data):
-        for s in data['eng']:
-            for word in s.split():
-                if word in self.eng_word2cnt:
-                    self.eng_word2cnt[word] += 1
-                else:
-                    self.eng_word2cnt[word] = 1
-        for s in data['fra']:
-            for word in s.split():
-                if word in self.fra_word2cnt:
-                    self.fra_word2cnt[word] += 1
-                else:
-                    self.fra_word2cnt[word] = 1
-        # Add the end of sentence token to each word count
-        self.eng_word2cnt['</s>'] = len(data['eng'])
-        self.fra_word2cnt['</s>'] = len(data['fra'])
-        # Sort according to word occurrence
-        sorted_eng = sorted(self.eng_word2cnt.items(), key=self.getCount, reverse=True)
-        sorted_fra = sorted(self.fra_word2cnt.items(), key=self.getCount, reverse=True)
-
-        print(sorted_eng[:10])
-        print('\n', sorted_fra[:10])
-
     def to_ascii(self, sentence):
         return ''.join(c for c in unicodedata.normalize('NFD', sentence) if unicodedata.category(c) != 'Mn')
 
     def preprocess(self):
         reader = self.read_data()
-        data = {}
-        data['eng'] = reader['lang1'].split('\n')
-        data['fra'] = reader['lang2'].split('\n')
-        print('Number of English sentences:', len(data['eng']))
-        print('Number of French sentences:', len(data['fra']))
-        print('[Example pair]:\n', data['eng'][3], '\n', data['fra'][3])
-        
-        self.build_dict(data) # Make dictionary from vocabs in dataset
-
+        eng = Language('eng')
+        fra = Language('fra')
+        eng.parseSentence(reader['lang1'])
+        fra.parseSentence(reader['lang2'])
+        print('Number of English sentences:', len(reader['lang1'].split('\n')))
+        print('Number of French sentences:', len(reader['lang2'].split('\n')))
         max_seq_len = 0
-        # Calculate the longest sequence length in both English and French sentences
-        for key, val in data.items():
-            for sentence in val:
-                length = len(sentence.split())
-                max_seq_len = max(max_seq_len, length)
-
+        # Calculate the longest sequence length from both English and French sentences
+        max_seq_len = max(eng.maxLen(), fra.maxLen())
+        print('Longest sequence in both langs: ', max_seq_len)
+        data = {}
         data['max_len'] = max_seq_len + 1
+        data['eng'] = eng.sentence2tensor(data['max_len'])
+        data['fra'] = fra.sentence2tensor(data['max_len'])
 
         return data
 
